0dafe3545f5797b6fddbac077abca399af64d10b
otouto3 <otoutoyeah@gmail.com>
2022-09-05 22:36:13 +0900

    初期プロジェクトの追加とフォルダの追加

---
+++ RecipeSaver/RecipeSaver/RecipeSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSaverApp.swift
+//  RecipeSaver
+//
+//  Created by ごつ on 2022/09/05.
+//
+
+import SwiftUI
+
+@main
+struct RecipeSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipeSaver/RecipeSaver/Views/Main/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecipeSaver
+//
+//  Created by ごつ on 2022/09/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
