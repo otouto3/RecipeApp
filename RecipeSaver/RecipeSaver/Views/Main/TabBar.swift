4f2e5a2f9d453cc3bb9cc153f313b9fdd54503e5
otouto3 <otoutoyeah@gmail.com>
2022-09-06 22:06:07 +0900

    TabBarの作成とTabBarの表示

---
+++ RecipeSaver/RecipeSaver/Views/Main/TabBar.swift
@@ -0,0 +1,41 @@
+//
+//  TabBar.swift
+//  RecipeSaver
+//
+//  Created by ごつ on 2022/09/06.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            CategoriesView()
+                .tabItem {
+                    Label("Categories", systemImage: "square.text.square")
+                }
+            NewRecipeView()
+                .tabItem {
+                    Label("New", systemImage: "plus")
+                }
+            FavoritesView()
+                .tabItem {
+                    Label("Favorites", systemImage: "heart")
+                }
+            SettingsView()
+                .tabItem {
+                    Label("Settings", systemImage: "gear")
+                }
+        }
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
