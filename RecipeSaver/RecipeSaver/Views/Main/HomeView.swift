27c8e15f81d265b8b187af72f8f61c60d2d6561f
otouto3 <otoutoyeah@gmail.com>
2022-09-08 01:08:15 +0900

    ホーム画面にレシピを表示させるようにした

--- RecipeSaver/RecipeSaver/Views/Main/HomeView.swift
+++ RecipeSaver/RecipeSaver/Views/Main/HomeView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         NavigationView {
-            List(Recipe.all) { recipe in
-                Text(recipe.name)
-                    .navigationTitle("Home")
+            ScrollView {
+                RecipeList(recipes: Recipe.all)
             }
+            .navigationTitle("My Recipes")
         }
         .navigationViewStyle(.stack)
     }
