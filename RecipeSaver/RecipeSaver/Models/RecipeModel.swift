f2fcbe9f3e7845e3e4f7e4977e7ed8b06b7ee361
otouto3 <otoutoyeah@gmail.com>
2022-09-08 01:07:59 +0900

    レシピ表示用のリストを作成

--- RecipeSaver/RecipeSaver/Models/RecipeModel.swift
+++ RecipeSaver/RecipeSaver/Models/RecipeModel.swift
@@ -64,6 +64,96 @@ extension Recipe {
             url: "https://cookpad.com/recipe/337397"
         ),
         Recipe(
+            name: "おいし～い☆うちの回鍋肉（ホイコーロー）",
+            image: "https://img.cpcdn.com/recipes/337397/894x1461s/420b2dd9ac964e456f6d761726d57b86?u=360623&p=1333186081",
+            description: "春キャベツを使っておいし～い☆回鍋肉をつくりませんか？テンメンジャンがなくても作れます。豆板醤を抜けばお子様向きになります。11月6日、100人話題入りさせていただきました。みなさん、ありがとうございますっ♬♫",
+            ingredients: "キャベツ",
+            directions: "頑張る",
+            category: "main",
+            datePublished: "2019-11-11",
+            url: "https://cookpad.com/recipe/337397"
+        ),
+        Recipe(
+            name: "おいし～い",
+            image: "https://img.cpcdn.com/recipes/337397/894x1461s/420b2dd9ac964e456f6d761726d57b86?u=360623&p=1333186081",
+            description: "春キャベツを使っておいし～い☆回鍋肉をつくりませんか？テンメンジャンがなくても作れます。豆板醤を抜けばお子様向きになります。11月6日、100人話題入りさせていただきました。みなさん、ありがとうございますっ♬♫",
+            ingredients: "キャベツ",
+            directions: "頑張る",
+            category: "main",
+            datePublished: "2019-11-11",
+            url: "https://cookpad.com/recipe/337397"
+        ),
+        Recipe(
+            name: "うちの回鍋肉（ホイコーロー）",
+            image: "https://img.cpcdn.com/recipes/337397/894x1461s/420b2dd9ac964e456f6d761726d57b86?u=360623&p=1333186081",
+            description: "春キャベツを使っておいし～い☆回鍋肉をつくりませんか？テンメンジャンがなくても作れます。豆板醤を抜けばお子様向きになります。11月6日、100人話題入りさせていただきました。みなさん、ありがとうございますっ♬♫",
+            ingredients: "キャベツ",
+            directions: "頑張る",
+            category: "main",
+            datePublished: "2019-11-11",
+            url: "https://cookpad.com/recipe/337397"
+        ),
+        Recipe(
+            name: "おいし～い☆うちの回鍋肉（ホイコーロー）",
+            image: "https://img.cpcdn.com/recipes/337397/894x1461s/420b2dd9ac964e456f6d761726d57b86?u=360623&p=1333186081",
+            description: "春キャベツを使っておいし～い☆回鍋肉をつくりませんか？テンメンジャンがなくても作れます。豆板醤を抜けばお子様向きになります。11月6日、100人話題入りさせていただきました。みなさん、ありがとうございますっ♬♫",
+            ingredients: "キャベツ",
+            directions: "頑張る",
+            category: "main",
+            datePublished: "2019-11-11",
+            url: "https://cookpad.com/recipe/337397"
+        ),
+        Recipe(
+            name: "おいし～い",
+            image: "https://img.cpcdn.com/recipes/337397/894x1461s/420b2dd9ac964e456f6d761726d57b86?u=360623&p=1333186081",
+            description: "春キャベツを使っておいし～い☆回鍋肉をつくりませんか？テンメンジャンがなくても作れます。豆板醤を抜けばお子様向きになります。11月6日、100人話題入りさせていただきました。みなさん、ありがとうございますっ♬♫",
+            ingredients: "キャベツ",
+            directions: "頑張る",
+            category: "main",
+            datePublished: "2019-11-11",
+            url: "https://cookpad.com/recipe/337397"
+        ),
+        Recipe(
+            name: "うちの回鍋肉（ホイコーロー）",
+            image: "https://img.cpcdn.com/recipes/337397/894x1461s/420b2dd9ac964e456f6d761726d57b86?u=360623&p=1333186081",
+            description: "春キャベツを使っておいし～い☆回鍋肉をつくりませんか？テンメンジャンがなくても作れます。豆板醤を抜けばお子様向きになります。11月6日、100人話題入りさせていただきました。みなさん、ありがとうございますっ♬♫",
+            ingredients: "キャベツ",
+            directions: "頑張る",
+            category: "main",
+            datePublished: "2019-11-11",
+            url: "https://cookpad.com/recipe/337397"
+        ),
+        Recipe(
+            name: "おいし～い☆うちの回鍋肉（ホイコーロー）",
+            image: "https://img.cpcdn.com/recipes/337397/894x1461s/420b2dd9ac964e456f6d761726d57b86?u=360623&p=1333186081",
+            description: "春キャベツを使っておいし～い☆回鍋肉をつくりませんか？テンメンジャンがなくても作れます。豆板醤を抜けばお子様向きになります。11月6日、100人話題入りさせていただきました。みなさん、ありがとうございますっ♬♫",
+            ingredients: "キャベツ",
+            directions: "頑張る",
+            category: "main",
+            datePublished: "2019-11-11",
+            url: "https://cookpad.com/recipe/337397"
+        ),
+        Recipe(
+            name: "おいし～い",
+            image: "https://img.cpcdn.com/recipes/337397/894x1461s/420b2dd9ac964e456f6d761726d57b86?u=360623&p=1333186081",
+            description: "春キャベツを使っておいし～い☆回鍋肉をつくりませんか？テンメンジャンがなくても作れます。豆板醤を抜けばお子様向きになります。11月6日、100人話題入りさせていただきました。みなさん、ありがとうございますっ♬♫",
+            ingredients: "キャベツ",
+            directions: "頑張る",
+            category: "main",
+            datePublished: "2019-11-11",
+            url: "https://cookpad.com/recipe/337397"
+        ),
+        Recipe(
+            name: "うちの回鍋肉（ホイコーロー）",
+            image: "https://img.cpcdn.com/recipes/337397/894x1461s/420b2dd9ac964e456f6d761726d57b86?u=360623&p=1333186081",
+            description: "春キャベツを使っておいし～い☆回鍋肉をつくりませんか？テンメンジャンがなくても作れます。豆板醤を抜けばお子様向きになります。11月6日、100人話題入りさせていただきました。みなさん、ありがとうございますっ♬♫",
+            ingredients: "キャベツ",
+            directions: "頑張る",
+            category: "main",
+            datePublished: "2019-11-11",
+            url: "https://cookpad.com/recipe/337397"
+        ),
+        Recipe(
             name: "（ホイコーロー）",
             image: "https://img.cpcdn.com/recipes/337397/894x1461s/420b2dd9ac964e456f6d761726d57b86?u=360623&p=1333186081",
             description: "春キャベツを使っておいし～い☆回鍋肉をつくりませんか？テンメンジャンがなくても作れます。豆板醤を抜けばお子様向きになります。11月6日、100人話題入りさせていただきました。みなさん、ありがとうございますっ♬♫",
